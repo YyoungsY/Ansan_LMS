6e11ee5828047834860263a1b8b8714f2f9086b9
YyoungsY <diddud1236@daum.net>
2025-04-11 15:28:23 +0900

    new project

---
+++ Ansan_LMS/Ansan_LMS/Ansan_LMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ansan_LMSApp.swift
+//  Ansan_LMS
+//
+//  Created by 양영 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Ansan_LMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ansan_LMS/Ansan_LMSTests/Ansan_LMSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ansan_LMSTests.swift
+//  Ansan_LMSTests
+//
+//  Created by 양영 on 4/11/25.
+//
+
+import Testing
+@testable import Ansan_LMS
+
+struct Ansan_LMSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
